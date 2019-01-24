da0637e6428387320800360a69afe02432339d4d
Akshay Bisht <abisht2@xavient.com>
2019-01-24 14:36:26 +0530

    Added src file

---
+++ src/my/org/fcopy.groovy
@@ -0,0 +1,21 @@
+package my.org
+
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
+
+public class test {
+    public static void main(String[] args) {
+ 
+        String source = "C:/Users/abisht2/Desktop/source";
+        File srcDir = new File(source);  
+        String destination = "C:/Users/abisht2/Desktop/destination";
+        File destDir = new File(destination);
+
+        try {
+          FileUtils.copyDirectory(srcDir, destDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
